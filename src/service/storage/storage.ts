5121684726b35f7bac50c713ce6a99e6c614e1e5
André Luiz <andreluiz.ddev@gmail.com>
2024-10-25 13:37:43 -0300

    divide abstraction upload image

--- src/service/storage/storage.ts
+++ src/service/storage/storage.ts
@@ -2,28 +2,40 @@ import confs from '@/constants/conf';
 import axios from 'axios';
 
 // Envio via base64 temporário devido a problemas no servidor.
+// Recebe uma File, e converte para base64;
 export async function uploadImageProfile(
 	file: File,
 	uuid_user: string,
 	uuid_profile: string,
 ) {
-	const formData = new FormData();
+	const base64String = await getBase64(file);
 
-	formData.append('file', file);
+	await uploadImageFormatBase64Profile(base64String, uuid_user, uuid_profile);
+}
 
-	const base64String = (await getBase64(file)).split(',')[1];
+// Envio via base64 temporário devido a problemas no servidor.
+export async function uploadImageFormatBase64Profile(
+	imageBase64String: string,
+	uuid_user: string,
+	uuid_profile: string
+) {
+	try {
+		imageBase64String = imageBase64String.split(',')[1]
 
-	await axios
-		.post(
-			`${confs.api_storage}/upload/${uuid_user}/${uuid_profile}`,
-			base64String,
-		)
-		.then((response) => {
-			console.log('Imagem enviada com sucesso!');
-		})
-		.catch((error) => {
-			console.error('Erro ao enviar imagem:', error);
-		});
+		await axios
+			.post(
+				`${confs.api_storage}/upload/${uuid_user}/${uuid_profile}`,
+				imageBase64String,
+			)
+			.then(() => {
+				console.log('Imagem enviada com sucesso!');
+			})
+			.catch((error) => {
+				console.error('Erro ao enviar imagem:', error);
+			});
+	} catch (err) {
+		console.log(err)
+	}
 }
 
 export function getPathImage(uuid_user: string, uuid_profile: string): string {
